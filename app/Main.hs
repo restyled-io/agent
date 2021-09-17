20bf39736fcb894544352fdd6729cd7adf75ecad
patrick brisbin <pbrisbin@gmail.com>
2021-09-17 14:43:18 -0400

    Re-implement as immortal

--- app/Main.hs
+++ app/Main.hs
@@ -4,17 +4,31 @@ module Main
 
 import RIO
 
-import Restyled.Agent
+import qualified Control.Immortal as Immortal
+import Restyled.Agent (withWebhook)
 import Restyled.Agent.AWS.LifecycleHooks
 import Restyled.Agent.App
 import Restyled.Agent.Options
+import Restyled.Agent.Restyler (processPullRequestEvent)
 
 main :: IO ()
 main = do
     options <- parseOptions
     withApp options $ do
-        mAgent <- withPendingLifecycleHook createAgent
+        mThreads <- withPendingLifecycleHook $ do
+            size <- oRestylerPoolSize <$> view optionsL
 
-        for_ mAgent $ \agent -> do
-            void $ async $ withTerminatingLifecycleHook $ terminateAgent agent
-            runAgent agent
+            for [1 .. size] $ \n -> do
+                let label = "restyle-" <> show n
+                Immortal.createWithLabel label
+                    $ \_ -> withWebhook processPullRequestEvent
+
+        for_ mThreads $ \threads -> do
+            withTerminatingLifecycleHook $ do
+                mDelay <- oTerminationDelay <$> view optionsL
+                for_ mDelay $ \delay -> do
+                    logInfo $ "Delay termination " <> displayShow delay <> "s"
+                    threadDelay $ fromIntegral delay * 1000000
+
+                logInfo "Stopping Agent"
+                liftIO $ traverse_ Immortal.stop threads
--- src/Restyled/Agent.hs
+++ src/Restyled/Agent.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE NamedFieldPuns #-}
-
+-- TODO: rename module
 module Restyled.Agent
-    ( Agent
-    , createAgent
-    , terminateAgent
-    , runAgent
+    ( withWebhook
     ) where
 
 import RIO hiding (timeout)
@@ -12,67 +8,8 @@ import RIO hiding (timeout)
 import Control.Lens (_2)
 import Data.Aeson
 import Data.Functor.Syntax ((<$$>))
-import RIO.Process
 import Restyled.Agent.Options
 import Restyled.Agent.Redis
-import Restyled.Agent.Restyler
-import Restyled.Agent.ThreadPool
-
-data Agent = Agent
-    { agentState :: IORef AgentState
-    , agentPool :: Pool
-    }
-
-data AgentState
-    = AgentRunning
-    | AgentStopping
-    | AgentStopped
-    deriving stock Show
-
-instance Display AgentState where
-    display = displayShow
-
-createAgent
-    :: (MonadIO m, MonadReader env m, HasLogFunc env, HasOptions env) => m Agent
-createAgent = do
-    size <- oRestylerPoolSize <$> view optionsL
-    Agent <$> newIORef AgentRunning <*> createPool size
-
-terminateAgent
-    :: (MonadIO m, MonadReader env m, HasLogFunc env, HasOptions env)
-    => Agent
-    -> m ()
-terminateAgent Agent { agentState } = do
-    mDelay <- oTerminationDelay <$> view optionsL
-    for_ mDelay $ \delay -> do
-        logInfo $ "Delaying termination " <> displayShow delay <> " second(s)"
-        threadDelay $ fromIntegral delay * 1000000
-
-    logInfo "Stopping Agent"
-    writeIORef agentState AgentStopping
-
-runAgent
-    :: ( MonadUnliftIO m
-       , MonadReader env m
-       , HasLogFunc env
-       , HasProcessContext env
-       , HasOptions env
-       , HasRedis env
-       )
-    => Agent
-    -> m ()
-runAgent agent@Agent { agentState, agentPool } = do
-    state <- readIORef agentState
-
-    case state of
-        AgentRunning -> do
-            handleAny (logError . displayShow)
-                $ withWebhook
-                $ withThread agentPool
-                . processPullRequestEvent
-            runAgent agent
-        AgentStopping -> writeIORef agentState AgentStopped
-        AgentStopped -> pure ()
 
 withWebhook
     :: ( MonadIO m
